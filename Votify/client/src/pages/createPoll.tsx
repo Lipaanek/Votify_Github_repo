992814af4623757f882e102598000442c983485c
Lipaanek <114157101+Lipaanek@users.noreply.github.com>
2025-11-01 10:40:31 +0100

    Update createPoll.tsx

--- Votify/client/src/pages/createPoll.tsx
+++ Votify/client/src/pages/createPoll.tsx
@@ -39,7 +39,7 @@ export default function CreatePollPage() {
   }, [navigate]);
 
   const fetchGroups = (userEmail: string) => {
-    fetch(`http://localhost:3000/api/info/groups?email=${encodeURIComponent(userEmail)}`, {
+    fetch(`http://voxplatform.fit.vutbr.cz:3000/api/info/groups?email=${encodeURIComponent(userEmail)}`, {
       credentials: 'include'
     })
       .then(res => res.json())
