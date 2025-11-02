e9988e12952f07fd303529f22346d7d79ba009c1
Lipaanek <lipovskymatyas@gmail.com>
2025-11-02 15:23:47 +0100

    bug fixes v5

--- Votify/client/vite.config.ts
+++ Votify/client/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://voxplatform.fit.vutbr.com:3000',
+        target: 'http://voxplatform.fit.vutbr.cz:3000',
         changeOrigin: true,
       },
     },
